abf03121fa0b314e64367072a539bfa7e139d2dd
Eric Hopkins <eric.on.tech@gmail.com>
2016-05-03 13:38:37 -0400

    Clean up tests with context blocks
    Added some context blocks to the tests, as well as removing
    redundant lines, with the intent of adding clarity to the
    tests, and reducing line noise.

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -24,93 +24,83 @@ describe 'A Vending Machine' do
     ].flatten!
   end
 
-  it 'should display "INSERT COIN" when the machine is empty' do
-    vending_machine = VendingMachine.new(@coin_set, nil, @bank)
-    expect(vending_machine.display).to eq('INSERT COIN')
+  describe 'when inserting money' do
+    before(:each) do
+      @vending_machine = VendingMachine.new(@coin_set, nil, @bank)
+    end
+
+    it 'should display "INSERT COIN" when the machine is empty' do
+      expect(@vending_machine.display).to eq('INSERT COIN')
+    end
+
+    it 'should accept a coin' do
+      expect(@vending_machine.insert(@nickel)).to eq nil
+      expect(@vending_machine.return_inserted).to eq [@nickel]
+    end
+
+    it 'should accept multiple coins' do
+      expect(@vending_machine.insert(@dime, @dime, @quarter)).to eq nil
+      expect(@vending_machine.return_inserted).to eq [@dime, @dime, @quarter]
+    end
+
+    it 'should put invalid coins into the coin return' do
+      @vending_machine.insert(@penny)
+      expect(@vending_machine.coin_return).to eq [@penny]
+    end
+
+    it "should show the user the amount they've entered on request" do
+      @vending_machine.insert(@nickel)
+      expect(@vending_machine.value).to eq 0.05
+    end
   end
 
-  it 'should accept a coin' do
-    vending_machine = VendingMachine.new(@coin_set, nil, nil)
-    result = vending_machine.insert(@nickel)
-    returned = vending_machine.return_inserted
-    expect(result).to eq nil
-    expect(returned).to eq [@nickel]
-  end
+  describe 'when buying an item' do
+    before(:each) do
+      @vending_machine = VendingMachine.new(@coin_set, @product_set, @bank)
+    end
 
-  it 'should accept multiple coins' do
-    vending_machine = VendingMachine.new(@coin_set, nil, nil)
-    result = vending_machine.insert(@dime, @dime, @quarter)
-    returned = vending_machine.return_inserted
-    expect(result).to eq nil
-    expect(returned).to eq [@dime, @dime, @quarter]
-  end
+    it 'should return nil if there are no products' do
+      expect(@vending_machine.buy('cola')).to eq nil
+    end
 
-  it 'should put invalid coins into the coin return' do
-    vending_machine = VendingMachine.new(@coin_set, nil, nil)
-    vending_machine.insert(@penny)
-    returned = vending_machine.coin_return
-    expect(returned).to eq [@penny]
-  end
+    it 'should return nil if requesting a non-existant product' do
+      @vending_machine.insert(@quarter, @quarter, @quarter, @quarter)
+      expect(@vending_machine.buy('widget')).to eq nil
+      expect(@vending_machine.display).to eq 'INSERT COIN'
+    end
 
-  it "should show the user the amount they've entered on request" do
-    vending_machine = VendingMachine.new(@coin_set, nil, nil)
-    vending_machine.insert(@nickel)
-    expect(vending_machine.value).to eq 0.05
-  end
+    it 'should return a product if enough money has been entered' do
+      @vending_machine.insert(@quarter, @quarter, @quarter, @quarter)
+      expect(@vending_machine.buy('cola')).to eq @product_set[0]
+      expect(@vending_machine.display).to eq 'THANK YOU'
+      expect(@vending_machine.display).to eq 'INSERT COIN'
+    end
 
-  it 'should return nil if there are no products' do
-    vending_machine = VendingMachine.new(@coin_set, [], nil)
-    bought = vending_machine.buy('cola')
-    expect(bought).to eq nil
-  end
+    it 'should show the price if enough money has not been entered' do
+      @vending_machine.insert(@quarter)
+      expect(@vending_machine.buy('cola')).to eq nil
+      expect(@vending_machine.display).to eq 'PRICE $1.0'
+      expect(@vending_machine.display).to eq 'INSERT COIN'
+    end
 
-  it 'should return nil if requesting a non-existant product' do
-    vending_machine = VendingMachine.new(@coin_set, @product_set, @bank)
-    vending_machine.insert(@quarter, @quarter, @quarter, @quarter)
-    bought = vending_machine.buy('widget')
-    expect(bought).to eq nil
-    expect(vending_machine.display).to eq 'INSERT COIN'
-  end
+    it 'should return coins when more were entered than needed' do
+      @vending_machine.insert(@quarter, @quarter, @quarter)
+      expect(@vending_machine.buy('candy')).to eq @product_set[2]
+      expect(@vending_machine.coin_return).to eq [@dime]
+      expect(@vending_machine.display).to eq 'THANK YOU'
+      expect(@vending_machine.display).to eq 'INSERT COIN'
+    end
 
-  it 'should return a product if enough money has been entered' do
-    vending_machine = VendingMachine.new(@coin_set, @product_set, @bank)
-    vending_machine.insert(@quarter, @quarter, @quarter, @quarter)
-    bought = vending_machine.buy('cola')
-    expect(bought).to eq @product_set[0]
-    expect(vending_machine.display).to eq 'THANK YOU'
-    expect(vending_machine.display).to eq 'INSERT COIN'
-  end
+    it 'should return nil when the selected item is out, and say "SOLD OUT"' do
+      @vending_machine.insert(@quarter, @quarter)
+      expect(@vending_machine.buy('chips')).to eq nil
+      expect(@vending_machine.display).to eq 'SOLD OUT'
+      expect(@vending_machine.display).to eq 'INSERT COIN'
+    end
 
-  it 'should show the price if enough money has not been entered' do
-    vending_machine = VendingMachine.new(@coin_set, @product_set, @bank)
-    vending_machine.insert(@quarter)
-    bought = vending_machine.buy('cola')
-    expect(bought).to eq nil
-    expect(vending_machine.display).to eq 'PRICE $1.0'
-    expect(vending_machine.display).to eq 'INSERT COIN'
-  end
-
-  it 'should return coins when more were entered than needed' do
-    vending_machine = VendingMachine.new(@coin_set, @product_set, @bank)
-    vending_machine.insert(@quarter, @quarter, @quarter)
-    bought = vending_machine.buy('candy')
-    expect(bought).to eq @product_set[2]
-    expect(vending_machine.coin_return).to eq [@dime]
-    expect(vending_machine.display).to eq 'THANK YOU'
-    expect(vending_machine.display).to eq 'INSERT COIN'
-  end
-
-  it 'should return nil when the selected item is out, and say "SOLD OUT"' do
-    vending_machine = VendingMachine.new(@coin_set, @product_set, @bank)
-    vending_machine.insert(@quarter, @quarter)
-    bought = vending_machine.buy('chips')
-    expect(bought).to eq nil
-    expect(vending_machine.display).to eq 'SOLD OUT'
-    expect(vending_machine.display).to eq 'INSERT COIN'
-  end
-
-  it 'should alert the user they need exact change' do
-    vending_machine = VendingMachine.new(@coin_set, @product_set, nil)
-    expect(vending_machine.display).to eq 'EXACT CHANGE ONLY'
+    it 'should alert the user they need exact change' do
+      vending_machine = VendingMachine.new(@coin_set, @product_set, nil)
+      expect(vending_machine.display).to eq 'EXACT CHANGE ONLY'
+    end
   end
 end
